7f66ce67e8c5ac898ca192ff7a976af29f40e135
durbi009 <75387319+durbi009@users.noreply.github.com>
2021-04-25 21:58:11 -0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,28 @@
+
+
+  console.log("background running");
+  
+  
+  //chrome.action.onClicked.addListener(askForAlarm);
+  function buttonClicked()
+  {
+      console.log("buttonclicked!");
+      console.log("now opening...");
+      //chrome.app.window.create('/user_inp.html')
+  }
+
+  chrome.action.onClicked.addListener(function () {
+    console.log("opening!")
+    chrome.tabs.create({ url: chrome.runtime.getURL("/website_directory/main.html") });
+});
+  //chrome.app.runtime.onLaunched.addListener(() => {
+    //chrome.app.window.create('/user_inp.html', {
+     // id: 'main',
+      //bounds: { width: 620, height: 500 }
+    //});
+  //});
+
+//chrome.app.runtime.onLaunched.addListener(createAlarm)
+
+
+
